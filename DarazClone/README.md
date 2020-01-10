d5f20317ba530e40b6faabc6ca4af8a42af6927d
mike-1994 <planetmukesh321@gmail.com>
2020-01-10 18:33:19 +0545

    4th Assignment Final

---
+++ DarazClone/README.md
@@ -0,0 +1,7 @@
+# DarazClone
+QWERasdfZXCV
+
+
+Firebase url: https://console.firebase.google.com/u/0/project/darazclone/overview
+
+Material Design Dependencies : https://github.com/rey5137/material
---
+++ DarazClone/app/src/main/java/com/example/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.Prevalent;
+
+import com.example.ecommerce.Model.Users;
+
+public class Prevalent
+{
+    // From this class we will be working on the forget password
+    // Remember me
+    // Type of features
+    // This package is created to get the current user who are using the app
+
+    private static Users currentOnlineUser;
+
+    // The below code is written to store the user phone i.e. the unqiue key to use this app contains all information of the user
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+
+}
